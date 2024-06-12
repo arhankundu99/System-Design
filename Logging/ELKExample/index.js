55a49ef7a030ad4cd322ef6e643b0e83be39d961
Arhan Kundu <arhankundu99@gmail.com>
2024-06-12 19:42:30 +0530

    Remove unnecessary files

--- Logging/ELKExample/index.js
+++
@@ -1,8 +0,0 @@
-const Logger = require('./logger')
-
-require('dotenv').config();
-
-const logger = new Logger(Math.random() * 100000, process.env.LOGSTASH_HOST);
-
-logger.info("my-service-12", "This service was able to fetch data");
-
--- Logging/README.md
+++
@@ -1,3 +0,0 @@
-# Logging Design
-
-![Logging Design](https://raw.githubusercontent.com/arhankundu99/System-Design/main/Logging/images/Logging%20Design.png)
